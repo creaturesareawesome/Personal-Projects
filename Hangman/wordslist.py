0b241978819d964ed78c276933662606c012552e
creaturesareawesome <148724815+creaturesareawesome@users.noreply.github.com>
2025-07-29 21:42:44 +0100

    Add files via upload

---
+++ Hangman/wordslist.py
@@ -0,0 +1,23 @@
+# words.py
+
+words = [
+    "aardvark", "alligator", "alpaca", "ant", "anteater", "antelope", "ape", "armadillo", 
+    "baboon", "badger", "beaver", "bee", "bison", "boar", "buffalo", "butterfly", "camel",
+    "capybara", "caribou", "cat", "caterpillar", "chamois", "cheetah", "chicken", "chimpanzee",
+    "chinchilla", "cough", "clam", "cobra", "cockroach", "cod", "crane", "crocodile", "crow",
+    "curlew", "deer", "dinosaur", "dog", "dogfish", "dolphin", "donkey", "dormouse", "duck",
+    "dugong", "dunlin", "eagle", "echidna", "eel", "eland", "elephant", "elk", "emu", "falcon",
+    "fish", "flamingo", "fly", "fox", "frog", "gaur", "gazelle", "gerbil", "gnat", "gnu", "goat",
+    "goldfish", "goose", "gorilla", "goshawk", "grouse", "guanaco", "gull", "hamster", "hare",
+    "hawk", "hedgehog", "hippopotamus", "hornet", "horse", "human", "hummingbird", "hyena", "ibex",
+    "ibis", "jackal", "jaguar", "jay", "kingfisher", "koala", "kookaburra", "koudou", "kudu", "lapwing",
+    "lark", "lemur", "leopard", "lion", "llama", "louse", "lyrebird", "magpie", "mallard", "manatee",
+    "mandrill", "mantis", "mosquito", "mouse", "mule", "narwhal", "newt", "nightingale", "octopus",
+    "okapi", "opossum", "oryx", "ostrich", "panda", "panther", "parrot", "partridge", "peafowl",
+    "pelican", "penguin", "pheasant", "pig", "pigeon", "porpoise", "quail", "quilt", "quokka", "rabbit",
+    "raccoon", "rail", "ram", "raven", "red-deer", "red-dollor", "sand-dollar", "sandpiper", "sardine",
+    "scorpion", "seahorse", "seal", "shark", "salamander", "salmon", "sand-dollar", "sardine",
+    "scorpion", "seahorse", "seal", "shark", "snake", "sparrow", "spider", "squirrel", "starling", "stingray",
+    "stoat", "stork", "swallow", "swine", "vulture", "wallaby", "walrus", "wasp", "weasel", "wolverine",
+    "wombat", "woodcock", "woodpecker", "worm", "wren", "yak", "zebra"
+]
